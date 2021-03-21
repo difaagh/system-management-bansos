2e235bda8a6c5ca2a718f6754f2bbaa788756098
difaagh <dhifa.aghisni@gmail.com>
2021-03-21 15:02:47 +0700

    initialize

---
+++ src/main/java/com/msb/app/management/system/bansos/helper/security.java
@@ -0,0 +1,18 @@
+package com.msb.app.management.system.bansos.helper;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author difaagh
+ */
+public class security {
+    
+    public boolean securityLogin(){
+        return true;
+    }
+}
---
+++ src/main/java/com/msb/app/management/system/bansos/main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.msb.app.management.system.bansos;
+
+import com.msb.app.management.system.bansos.screen.Login;
+
+/**
+ *
+ * @author mac
+ */
+public class main {
+    	public static void main(String[] args) {
+            Login login = new Login();
+            login.setVisible(true);
+	}
+
+}
